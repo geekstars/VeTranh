04528275e77222884b12cd7b6d9128d15ac07f0a
Hoàng Minh Thành <hoangminhthanh@hoangs-macbook-pro-5.local>
2016-09-19 10:11:17 +0700

    Initial Commit

---
+++ VeTranh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeTranh
+//
+//  Created by Hoàng Minh Thành on 9/19/16.
+//  Copyright © 2016 Hoàng Minh Thành. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
